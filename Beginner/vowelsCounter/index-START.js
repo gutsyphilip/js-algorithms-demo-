f809f5b1470334efbc9e0c4686851445e72664a8
Justine Philip <philip.obosi@eng.uniben.edu>
2019-01-08 03:50:47 +0100

    cleaned up start file

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -1,21 +1,10 @@
-/*
-An iterative approach to counting the number of vowels in a
-string of text.
+/* CHALLENGE
+Given a string of text, return the number of vowels found within the text
 */
 
-const vowels = ["a", "e", "i", "o", "u"]
 
 function vowelsCounter(text) {
-    // Search text with Regex and store all matching instances 
-    let matchingInstances = text.match(/[aeiou]/gi);
-
-    // Check if matching instances exist then calculate length
-    if (matchingInstances) {
-        // Return number of vowels
-        return matchingInstances.length
-    } else {
-        return 0
-    }
+   
 }
 
 
